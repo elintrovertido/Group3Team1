52705c8144802e21f646518ffba6513a1d849303
elintrovertido <angadipetasanthu@gmail.com>
2025-07-28 16:43:06 +0530

    Added Splunk in Notification and Product service.

--- notificationservice/src/main/java/com/ecommerce/notificationservice/service/KafkaNotificationProducer.java
+++ notificationservice/src/main/java/com/ecommerce/notificationservice/service/KafkaNotificationProducer.java
@@ -4,21 +4,21 @@ import com.ecommerce.notificationservice.dto.NotificationEvent;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
-public class KafkaNotificationProducer {
-
-    private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
-
-    public KafkaNotificationProducer(KafkaTemplate<String, NotificationEvent> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void sendToOrderTopic(NotificationEvent event) {
-        kafkaTemplate.send("order-events", event);
-    }
-
-    public void sendToPaymentTopic(NotificationEvent event) {
-        kafkaTemplate.send("payment-events", event);
-    }
-}
-
+//@Service
+//public class KafkaNotificationProducer {
+//
+//    private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
+//
+//    public KafkaNotificationProducer(KafkaTemplate<String, NotificationEvent> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    public void sendToOrderTopic(NotificationEvent event) {
+//        kafkaTemplate.send("order-events", event);
+//    }
+//
+//    public void sendToPaymentTopic(NotificationEvent event) {
+//        kafkaTemplate.send("payment-events", event);
+//    }
+//}
+//
