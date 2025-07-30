0443f66547932d0e761da492f51c99770e823366
elintrovertido <angadipetasanthu@gmail.com>
2025-07-30 10:18:25 +0530

    Refactored Code.

--- notificationservice/src/main/java/com/ecommerce/notificationservice/service/NotificationConsumer.java
+++ notificationservice/src/main/java/com/ecommerce/notificationservice/service/NotificationConsumer.java
@@ -9,29 +9,29 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-//
-//@Service
-//@Slf4j
-//public class NotificationConsumer {
-//
-//    NotificationService notificationService;
-//
-//    public NotificationConsumer(NotificationService notificationService){
-//        this.notificationService = notificationService;
-//    }
-//
-//    @KafkaListener(groupId = "notification-group", topics = {"order-events", "payment-events"})
-//    public void listen(ConsumerRecord<String, NotificationEvent> record){
-//        NotificationEvent notificationreq = record.value();
-//        NotificationRequest notification = NotificationRequest.builder()
-//                .recipient(notificationreq.getRecipient())
-//                .message(notificationreq.getMessage())
-//                .notificationType(Notification.NotificationType.valueOf(notificationreq.getType()))
-//                .notificationPriority(Notification.NotificationPriority.valueOf(notificationreq.getPriority()))
-//                .scheduledTime(notificationreq.getScheduledTime())
-//                .build();
-//        log.info(notification.toString());
-//        notificationService.sendNotification(notification);
-//
-//    }
-//}
+
+@Service
+@Slf4j
+public class NotificationConsumer {
+
+    NotificationService notificationService;
+
+    public NotificationConsumer(NotificationService notificationService){
+        this.notificationService = notificationService;
+    }
+
+    @KafkaListener(groupId = "notification-group", topics = {"order-events", "payment-events"})
+    public void listen(ConsumerRecord<String, NotificationEvent> record){
+        NotificationEvent notificationreq = record.value();
+        NotificationRequest notification = NotificationRequest.builder()
+                .recipient(notificationreq.getRecipient())
+                .message(notificationreq.getMessage())
+                .notificationType(Notification.NotificationType.valueOf(notificationreq.getType()))
+                .notificationPriority(Notification.NotificationPriority.valueOf(notificationreq.getPriority()))
+                .scheduledTime(notificationreq.getScheduledTime())
+                .build();
+        log.info(notification.toString());
+        notificationService.sendNotification(notification);
+
+    }
+}
